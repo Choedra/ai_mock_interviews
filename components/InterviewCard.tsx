42074c77cc0c5b2a6505b12f662b111ebce8b04b
Choedra <choedra999@gmail.com>
2025-03-26 21:19:42 +0600

    implement interview session

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -5,7 +5,8 @@ import { getRandomInterviewCover } from '@/lib/utils';
 import { Button } from './ui/button';
 import Link from 'next/link';
 import DisplayTechIcons from './DisplayTechIcons';
-const InterviewCard = ({ interviewId, userId, role, type, techstack,
+
+const InterviewCard = ({ id, userId, role, type, techstack,
     createdAt }: InterviewCardProps) => {
         const feedback = null as Feedback | null;
         const normalizedType = /mix/gi.test(type) ? 'Mixed' : type;
@@ -48,7 +49,7 @@ const InterviewCard = ({ interviewId, userId, role, type, techstack,
                 <DisplayTechIcons techStack={techstack}/>
 
                 <Button className='btn-primary'>
-                    <Link href={feedback? `/interview/${interviewId}/feedback`: `/interview/${interviewId}`}>
+                    <Link href={feedback? `/interview/${id}/feedback`: `/interview/${id}`}>
                         {feedback ? 'Check Feedback' : 'View Interview'}
                     </Link>
                 </Button>
